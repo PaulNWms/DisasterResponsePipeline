f199aa37577319cb293c5ada6778cf31fb9ec4ce
Paul Williams <paulwilliams_redmond@hotmail.com>
2019-07-31 12:11:45 +0000

    feat: add files

---
+++ models/train_classifier.py
@@ -0,0 +1,101 @@
+import sys
+import re
+import pandas as pd
+import nltk
+import pickle
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk import pos_tag, ne_chunk
+from nltk.stem.porter import PorterStemmer
+from nltk.sem import relextract
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import classification_report
+from sklearn.naive_bayes import MultinomialNB
+from past.builtins import execfile
+
+
+execfile('models/transformers.py')
+
+nltk.download(['punkt', 'wordnet', 'stopwords', 'averaged_perceptron_tagger', 'maxent_ne_chunker', 'words'])
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///pauls_messages.db')
+    df = pd.read_sql_table('messages', engine)
+    X = df.message
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_names = list(Y)[1:]
+    return X, Y, category_names
+
+def build_model():
+    return Pipeline([
+        ('features', FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('tok_cnt', TokenCountExtractor()),
+            ('upper_pct', UpperCaseExtractor()),
+            ('ent_cnt', EntityCountExtractor()),
+        ])),
+
+        ('clf', MultiOutputClassifier(MultinomialNB())),
+    ])
+
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    Y_pred = model.predict(X_test)
+    indices = range(Y_test.shape[1])
+    for col, col_name in zip(indices, category_names):
+        test = Y_test.values[:, col]
+        pred = Y_pred[:, col]
+        sep = col_name + ' -----------------------------------------'
+        rpt = classification_report(test, pred)
+        print(sep)
+        print(rpt)
+
+
+def save_model(model, model_filepath):
+    pipeline_pkl = open(model_filepath, 'wb')
+    pickle.dump(model, pipeline_pkl)
+    pipeline_pkl.close()
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/transformers.py
@@ -0,0 +1,83 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+def tokenize(text):
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    tokens = [w for w in tokens if w not in stopwords.words("english")]
+    tagged_tokens = pos_tag(tokens)
+    
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+
+    for tagged_token in tagged_tokens:
+        token = tagged_token[0]
+        tag = tagged_token[1][0].lower()
+        if tag in ('a', 'n', 'r', 'v'):
+            clean_tok = lemmatizer.lemmatize(token, pos=tag)
+        else:
+            clean_tok = lemmatizer.lemmatize(token)
+
+        clean_tokens.append(clean_tok)
+        
+    stemmed_tokens = [PorterStemmer().stem(w) for w in clean_tokens]
+    return stemmed_tokens
+
+class TokenCountExtractor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    
+    def my_operation(self, text):
+        return len(word_tokenize(text))
+
+    def transform(self, X):
+        result = [self.my_operation(text) for text in X]
+        return pd.DataFrame(result, columns=['tok_cnt'])
+    
+class UpperCaseExtractor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def my_operation(self, text):
+        length = len(text.strip())
+        if length == 0:
+            return 0
+        uppercase = sum(1 for c in text if c.isupper())
+        return uppercase / length
+    
+    def transform(self, X):
+        result = [self.my_operation(text) for text in X]
+        return pd.DataFrame(result, columns=['upper_pct'])
+
+class EntityCountExtractor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    
+    def extract_entity_counts_helper(self, t):
+        if hasattr(t, 'label') and t.label:
+            if t.label() == 'GPE':
+                self.gpe += 1
+            elif t.label() == 'PERSON':
+                self.person += 1
+            elif t.label() == 'ORGANIZATION':
+                self.org += 1
+
+            for child in t:
+                self.extract_entity_counts_helper(child)
+
+    def extract_entity_counts(self, message):
+        self.gpe = self.person = self.org = 0
+        tree = ne_chunk(pos_tag(word_tokenize(message)))
+        self.extract_entity_counts_helper(tree)
+        
+    def my_operation(self, text):
+        self.extract_entity_counts(text)
+        return self.gpe, self.person, self.org
+
+    def transform(self, X):
+        result = [self.my_operation(text) for text in X]
+        return pd.DataFrame(result, columns=['gpe_cnt', 'person_cnt', 'org_cnt'])
+
